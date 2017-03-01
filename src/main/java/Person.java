f6109e1f97d7a2e5c903a6160edaa4ec15017811
Buddhika Karunatilleke <buddhika.karunatilleke@trycake.com>
2017-03-01 16:43:43 +0530

    Added StringUtils.java

---
+++ src/main/java/Person.java
@@ -0,0 +1,5 @@
+/**
+ * Created by buddhikak on 3/1/17.
+ */
+public class Person {
+}
---
+++ src/main/java/StringUtil.java
@@ -0,0 +1,26 @@
+
+/**
+ * @author : Padmaka Wijayagoonawardena
+ * @date : 3/1/17
+ */
+public class StringUtil {
+
+    public static void main(String[] args) {
+
+        StringUtil stringUtil = new StringUtil();
+        System.out.println(stringUtil.checkStringAndPrint("name"));
+    }
+
+    public Person checkStringAndPrint(String str) {
+        Person person = null;
+
+        if (str == null) {
+            throw new IllegalArgumentException();
+        } else {
+            person = new Person();
+            person.setName(str);
+        }
+
+        return person;
+    }
+}
