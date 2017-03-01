8e4ff21fa5ddd0877ca67bac670c163d0506b8ef
Buddhika Karunatilleke <buddhika.karunatilleke@trycake.com>
2017-03-01 17:31:34 +0530

    Person bean added

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,25 @@
+
 /**
- * Created by buddhikak on 3/1/17.
+ * @author : Padmaka Wijayagoonawardena
+ * @date : 3/1/17
  */
 public class Person {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
